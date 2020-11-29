e6ad3fcdf280e9900448c469569dcdfb82218e93
hendersonanalytics <everett@hendersonanalytics.com>
2020-11-28 23:16:37 -0600

    end displaying home component data using angularfire

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {Observable} from "rxjs";
-import {map} from "rxjs/operators";
+import {Course} from '../model/course';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { CourseComponent } from 'app/course/course.component';
 
 
 
@@ -12,15 +14,34 @@ import {map} from "rxjs/operators";
 })
 export class HomeComponent implements OnInit {
 
+  public courses$: Observable<Course[]>;
+  public beginnerCourses$: Observable<Course[]>;
+  public advancedCourses$: Observable<Course[]>;
 
-    constructor() {
+  constructor(private db: AngularFirestore) {
 
-    }
+  }
 
-    ngOnInit() {
+  ngOnInit() {
+    this.courses$ = this.db.collection('courses').snapshotChanges()
+      .pipe(
+        map(snaps => {
+          return snaps.map(snap => {
+            return <Course> {
+              id: snap.payload.doc.id,
+              ...snap.payload.doc.data() as {}
+            };
+          });
+        })
+    );
 
+    this.beginnerCourses$ = this.courses$.pipe(
+      map(courses => courses.filter(course => course.categories.includes('BEGINNER')))
+    );
 
-
+    this.advancedCourses$ = this.courses$.pipe(
+      map(courses => courses.filter(course => course.categories.includes('ADVANCED')))
+    );
     }
 
 }
