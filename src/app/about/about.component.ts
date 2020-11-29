fa78505ef9b201485d169630ea53f974d7c5f678
hendersonanalytics <everett@hendersonanalytics.com>
2020-11-28 22:58:13 -0600

    end snapshotchanges vs statechanges

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Course } from 'app/model/course';
 
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
@@ -15,9 +16,15 @@ export class AboutComponent implements OnInit {
   constructor(private db: AngularFirestore) { }
 
   ngOnInit() {
-    this.db.collection('courses').valueChanges()
-      .subscribe(console.log); // observable is live; BE changes are immediately reflected here
-
+    this.db.collection('courses').snapshotChanges()
+      .subscribe(snaps => {
+        const courses: Course[] = snaps.map(snap => {
+          return <Course> {
+            id: snap.payload.doc.id,
+            ...snap.payload.doc.data() as {}
+          };
+        });
+      });
   }
 
 }
