5edeb500d5d08c137faecfa99e1c70c5a48bb8ef
hendersonanalytics <everett@hendersonanalytics.com>
2020-11-29 23:15:06 -0600

    end pagination and loading indicator

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,7 +1,7 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Course} from '../model/course';
-import {tap} from 'rxjs/operators';
+import {finalize, tap} from 'rxjs/operators';
 import {Observable} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import { CoursesService } from 'app/services/courses.service';
@@ -16,6 +16,8 @@ export class CourseComponent implements OnInit {
 
   course: Course;
   lessons: Lesson[];
+  lastPageLoaded = 0;
+  loading = false;
 
   displayedColumns = ['seqNo', 'description', 'duration'];
   dataSource: any;
@@ -26,16 +28,21 @@ export class CourseComponent implements OnInit {
     ) { }
 
   ngOnInit() {
+    this.loading = true;
+
     this.course = this.route.snapshot.data['course'];
+
     this.coursesService.findLessons(this.course.id)
-      .subscribe(lessons => {
-        this.lessons = lessons;
-      });
+      .pipe(finalize(() => { this.loading = false; }))
+      .subscribe(lessons => { this.lessons = lessons; });
   }
 
   loadMore() {
+    this.lastPageLoaded++;
+    this.loading = true;
 
+    this.coursesService.findLessons(this.course.id, 'asc', this.lastPageLoaded)
+      .pipe(finalize(() => { this.loading = false; }))
+      .subscribe(lessons => { this.lessons = this.lessons.concat(lessons); });
   }
-
-
 }
