79e096e8d45d2bbefafb55bdcb07ea5073f53f9a
hendersonanalytics <everett@hendersonanalytics.com>
2020-11-29 22:36:30 -0600

    end querying nested FS collection

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -4,6 +4,7 @@ import {Course} from '../model/course';
 import {tap} from 'rxjs/operators';
 import {Observable} from 'rxjs';
 import {Lesson} from '../model/lesson';
+import { CoursesService } from 'app/services/courses.service';
 
 
 @Component({
@@ -14,24 +15,22 @@ import {Lesson} from '../model/lesson';
 export class CourseComponent implements OnInit {
 
   course: Course;
+  lessons: Lesson[];
 
   displayedColumns = ['seqNo', 'description', 'duration'];
-
   dataSource: any;
 
-
   constructor(
-    private route: ActivatedRoute) {
-
-
-  }
+    private route: ActivatedRoute,
+    private coursesService: CoursesService
+    ) { }
 
   ngOnInit() {
-
     this.course = this.route.snapshot.data['course'];
-
-
-
+    this.coursesService.findLessons(this.course.id)
+      .subscribe(lessons => {
+        this.lessons = lessons;
+      });
   }
 
   loadMore() {
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import {first, map, take} from 'rxjs/operators';
+import { OrderByDirection } from '@firebase/firestore-types';
 
 import { Course } from 'app/model/course';
 import { Observable } from 'rxjs';
 
 import {convertSnaps} from '../utils/db-utils'
+import { Lesson } from 'app/model/lesson';
 
 @Injectable({
   providedIn: 'root'
@@ -28,6 +30,19 @@ export class CoursesService {
       .pipe(
         map(courses => convertSnaps(courses)),
         map(courses => courses.length === 1 ? courses[0] : undefined),
+        take(1)
+      );
+  }
+
+  findLessons(courseId: string, sortOrder: OrderByDirection = 'asc',
+  pageNumber = 0, pageSize = 3): Observable<Lesson[]> {
+    return this.db.collection(`courses/${courseId}/lessons`,
+    ref => ref.orderBy('seqNo', sortOrder)
+      .limit(pageSize)
+      .startAfter(pageNumber * pageSize)
+    ).snapshotChanges()
+      .pipe(
+        map(lessons => convertSnaps(lessons)),
         take(1)
       );
   }
